944a94fe7c561a4833dfc52c465fa2754238640b
Damián morales <60529414+dam788@users.noreply.github.com>
2021-07-17 17:35:22 -0300

    sin useCallback

--- src/hooks/fetchFunction.js
+++ src/hooks/fetchFunction.js
@@ -1,12 +1,16 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
 
 // los custom hook la mayoria de las veces te devuelven un array o un objeto,
 // en este caso lo unico que hice fue actualizar el estado del componente
 // fon el fetch....
 
 export const useFetch = (url, setData) => {
-  const fetchURL = useCallback(() => {
+
+  
+  
+  useEffect(() => {
     if (url === '') return null;
+    
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
@@ -15,12 +19,9 @@ export const useFetch = (url, setData) => {
       })
       .catch((err) => {
         console.log(err);
-      });
-  }, [setData, url]);
+      })
 
-  useEffect(() => {
-    fetchURL();
-  }, [fetchURL, url]);
+  }, [setData, url]);
 
   // aca te devolveria un array ejemplo:
   // return[data,error,loading];
